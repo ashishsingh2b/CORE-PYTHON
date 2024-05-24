846b68ac9831212d091230dbbb8f1ec44c096753
Ashish Singh <ashishsingh2b@gmail.com>
2024-05-24 18:34:37 +0530

    while commit

---
+++ 9whileloop/while.py
@@ -0,0 +1,21 @@
+# n = 1
+# while n<=100:
+#     print(n)
+#     n +=1
+
+
+# n=1
+# while n<=10:
+#     print("Outer loop",n)
+#     n+=1
+#     a=1
+#     while a<=5:
+#          print("Inner loop",a)
+#          a+=1
+
+# n=1 
+# while n<=20:
+#     n+=1
+#     print(n)
+# else:
+#     print("Condition Satisfaction Completed")
