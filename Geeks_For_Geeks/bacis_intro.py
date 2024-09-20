4cc27dfc77a0e32f2b4616c5224843b690740da7
Ashish Singh <ashishsingh2b@gmail.com>
2024-09-20 14:06:02 +0530

    quiz commit

---
+++ Geeks_For_Geeks/bacis_intro.py
@@ -0,0 +1,1 @@
+print("Python is High level Language Programming language")
---
+++ Geeks_For_Geeks/quiz.py
@@ -0,0 +1,17 @@
+
+#Question 16: What is the output of the following program?
+
+a= "Ashish"
+b=13
+b=str(b) 
+print(a+b) #to concreat have to convert the data type into same data type   
+
+#Question 4: What is the output of the following program?
+
+set1 = {1,2,3}
+newset=set1.add(4)
+print(newset) #set1.add() returns None
+
+
+
+
